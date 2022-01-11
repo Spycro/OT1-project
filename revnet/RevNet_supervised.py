565b459c717798f31449f34def133eaff341d457
Charles JAVERLIAT <charles.javerliat@gmail.com>
2022-01-11 11:32:18 +0100

    Revert "test: freeze features params"
    This reverts commit 6917a66b183aa5e974e9210a5a8336ce11d268d3.

--- revnet/RevNet_supervised.py
+++ revnet/RevNet_supervised.py
@@ -149,10 +149,6 @@ supervised_model.classifier = nn.Sequential(
                 nn.ReLU(),
             ).to(device)
 
-# Freeze features paramers
-for param in supervised_model.features.parameters():
-    param.requires_grad = False
-
 print("Started training...")
 train(supervised_model)
 print("Finished training")
